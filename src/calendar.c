719fb48978f5a3142aa2c889a463fc3b0ec1c166
Sadie <sfreisthler@gmail.com>
2023-04-25 14:50:50 -0400

    Added function to calculate day of date

--- src/calendar.c
+++ src/calendar.c
@@ -19,6 +19,12 @@ int daysInMonth(int monthNum, int year) {
     return days;
 }
 
+int dayOfWeek(int d, int m, int y) {
+    static int t[] = {0,3,2,5,0,3,5,1,4,6,2,4};
+    y -= m < 3;
+    return (y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
+}
+
 char* monthName(int monthNum) {
     char *month;
     switch (monthNum) {
@@ -61,11 +67,13 @@ char* monthName(int monthNum) {
     return month;
 }
 
-void printCal(int year) {
-    printf("\n\n    %d\n\n",year);
+void printCal(int y) {
+    printf("\n\n    %d\n\n",y);
     for (int i = 1; i <= 12; i++) {
 	
 	printf("%s\n",monthName(i));
+	int day = dayOfWeek(1,i,y);
+	printf("%d\n", day);
     }
 }
 
